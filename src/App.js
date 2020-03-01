9c3c5e6fad4ff61d0801cf1f599b116bbbb6629e
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2020-03-01 19:44:18 +0530

    Featured login,signup button and router navigation.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import { HashRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Login' component={Login} />
+        <Route exact path='/SignUp' component={SignUp} />
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
+import {NavLink } from "react-router-dom";
+
 
 function Home() {
   return (
     <div className="Home">
       <div id='header' >
-        <div id='login' >Login</div>
+        <NavLink to={"/SignUp"} exact id='signup' className='btn' >SignUp</NavLink>
+        <NavLink to={"/Login"} exact id='login' className='btn' >Login</NavLink>
       </div>
     </div>
   );
