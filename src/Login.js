9cfffc93f087e5490744dd2c01366cede1cc66cb
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2020-03-01 21:53:01 +0530

    Customised login page part 1

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Login.css';
 
+
+const submit = ()=>{
+
+}
+
 function Login() {
+  const [creds,setCreds] = useState({'username':'','password':''})
+
   return (
-    <div className="Login">
-      
+    <div className="login">
+      {/* <form onSubmit={submit} >
+        <label>
+          UserName : 
+          <input type='text' value = {creds.username} onChange={(event)=>{
+            setCreds({...creds, 'username' : event.target.value })
+            
+          }} />
+        </label>
+      </form> */}
+      <div id='form' >
+        <label className = 'label'>Username</label> <br/>
+        <input type="text" id="fname" name="username" placeholder="Username / Email...." />
+
+      </div>
     </div>
   );
 }
