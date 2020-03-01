e425145b43688a0b7f6d203809ea99d16ef86d28
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2020-03-01 18:00:37 +0530

    Reconfigured the starting. Initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <div id='header' >
+        <div id='login' >Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
